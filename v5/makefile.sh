4ee22f75abb4b8da46e0ad4fa9434625c9410b72
BenjaminCleon <70745884+BenjaminCleon@users.noreply.github.com>
2022-09-22 10:45:39 +0200

    Delete makefile.sh

--- v5/makefile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gcc -c generateur/generateur.c
-gcc -c suite/suite.c -Igenerateur
-gcc -c principale/app.c -Isuite -Igenerateur
-
-gcc -o generateur/generateur.o suite/suite.o principale/app.o -o app.exe
-
-./principale/app.exe 
